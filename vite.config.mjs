d71de0f8da17d3659f40cd470cafc5b1a6d4ea41
AMAJ11 <155218090+AMAJ11@users.noreply.github.com>
2024-06-28 15:31:35 +0300

    Update vite.config.mjs

--- vite.config.mjs
+++ vite.config.mjs
@@ -11,7 +11,7 @@ import { fileURLToPath, URL } from "node:url";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/contact/",
+  base: "/",
   plugins: [
     VueRouter(),
     Vue({
